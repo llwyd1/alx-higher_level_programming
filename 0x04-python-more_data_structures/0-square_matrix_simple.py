cd242f0141b3eeeccc043c554ebf7154597b8438
llwyd1 <llwydmodes@gmail.com>
2022-07-11 22:55:39 +0200

    Added task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 
 def square_matrix_simple(matrix=[]):
 
-    square = list(map(lambda x: list(map(lambda y : y**2, x)), matrix))
+    square = list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
     return square
