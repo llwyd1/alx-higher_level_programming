91e1378c716176be61cc09d11e3b64bf6dabcc3c
Lloyd <llwydmodes@gmail.com>
2024-03-18 21:14:18 +0200

    Added task 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import hidden_4
+
+    list = dir(hidden_4)
+    for i in range(0, len(list)):
+        if list[i][0:2] != "__":
+            print("{}".format(list[i]))
