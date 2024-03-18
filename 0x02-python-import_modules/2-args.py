3d0ade9daf8bd3ae4b5d32d74162465bef33caa8
Lloyd <llwydmodes@gmail.com>
2024-03-18 21:01:17 +0200

    Added task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    num = len(argv) - 1
+    if num == 0:
+        print("{} arguments.".format(num))
+    elif num == 1:
+        print("{} argument:".format(num))
+    else:
+        print("{} arguments:".format(num))
+
+    for i in range(1, num + 1):
+        print("{}: {}".format(i, argv[i]))
