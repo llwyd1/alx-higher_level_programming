3d75723e0e5f23e30a2b9735f627f5e3e2e23210
Lloyd <llwydmodes@gmail.com>
2024-05-23 11:44:24 +0200

    Added task 0

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,5 @@
+-- lists all the privileges for the MySQL users
+-- lists all the privileges for one particular user in a database
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- lists all the privileges for one particular user of a database
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
