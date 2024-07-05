fb85bbc00bbf0127a9975b8472957300c9f76187
Lloyd <llwydmodes@gmail.com>
2024-07-05 12:35:58 +0200

    Added task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Takes in a URL, sends a request and displays the value of a variable in the header of the response"""
+
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+
+    url = sys.argv[1]
+
+    with urllib.request.urlopen(url) as response:
+        header = response.headers.get('X-Request-Id')
+        print(header)
