0e28cc22f8e11d104aa10e673e1496122daff394
Lloyd <llwydmodes@gmail.com>
2024-07-05 12:37:43 +0200

    Corrected task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Takes in a URL, sends a request and displays the value of a variable in the header of the response"""
+"""Takes in a URL, sends a request and displays the value of a
+variable in the header of the response"""
 
 if __name__ == '__main__':
     import urllib.request
